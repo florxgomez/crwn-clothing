e2114681c07551fcd897a7cf3735efdf584088a2
Flor <florxgomez@gmail.com>
2019-10-28 02:14:50 -0300

    add and remove item from checkout

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -8,3 +8,13 @@ export const addItem = item => ({
   type: CartTypes.ADD_ITEM,
   payload: item,
 });
+
+export const clearItemFromCart = item => ({
+  type: CartTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
+export const removeItem = item => ({
+  type: CartTypes.REMOVE_ITEM,
+  payload: item
+})
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,5 +1,5 @@
 import CartTypes from "./cartTypes";
-import { addItemToCart } from "./cartUtils";
+import { addItemToCart, removeItemFromCart } from "./cartUtils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -18,6 +18,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        ),
+      };
+    case CartTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
     default:
       return state;
